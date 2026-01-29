59b21764dd46006b90a2ae50fc88ded6261afe16
Drue Gupta <1031762@my.scccd.edu>
2026-01-29 12:55:05 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,34 +2,6 @@
 #include "Sphere.h"
 using namespace std;
 
-double Sphere::getRadius()
-{
-	return radius;
-}
-
-void Sphere::setRadius(double radius)
-{
-	if (radius > 0)
-		this->radius = radius;
-	else
-		cerr << "no negatives" << endl;
-}
-
-double Sphere::surfaceArea()
-{
-	return (4 * 3.14 * radius * radius);
-}
-
-double Sphere::volume()
-{
-	return ((4 / 3) * 3.14 * radius * radius * radius);
-}
-
-Sphere::Sphere()
-{
-	this->radius = 0;
-}
-
 int main(string sphere)
 {
 	Sphere a;
