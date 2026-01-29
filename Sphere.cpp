9fdf084d7621453f8c0aa9e1131ffea46bbc7a9d
Drue Gupta <1031762@my.scccd.edu>
2026-01-29 12:56:05 -0800

    Update Sphere.cpp

--- Sphere.cpp
+++ Sphere.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include "Shpere.h"
+using namespace std;
 
+double Sphere::getRadius()
+{
+	return radius;
+}
+
+void Sphere::setRadius(double radius)
+{
+	if (radius > 0)
+		this->radius = radius;
+	else
+		cerr << "no negatives" << endl;
+}
+
+double Sphere::surfaceArea()
+{
+	return (4 * 3.14 * radius * radius);
+}
+
+double Sphere::volume()
+{
+	return ((4 / 3) * 3.14 * radius * radius * radius);
+}
+
+Sphere::Sphere()
+{
+	this->radius = 0;
+}
